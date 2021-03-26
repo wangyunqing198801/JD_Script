2ae84abd9b53600b56dfa56b1b789d6d7ced79a5
Shineslee <46121976+Shineslee@users.noreply.github.com>
2021-03-26 09:27:24 +0800

    Update jd_bean_home.js

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -26,7 +26,6 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const helpAuthor = false; // 是否帮助作者助力，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
@@ -111,9 +110,7 @@ const JD_API_HOST = 'https://api.m.jd.com/';
   })
 
 async function jdBeanHome() {
-  if(helpAuthor){
-    shuye72
-  }
+  await shuye72()
   $.doneState = false
   // for (let i = 0; i < 3; ++i) {
   //   await doTask2()
