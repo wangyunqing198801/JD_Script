333879a49872ed85f59861f46867657f1d1a5b7c
Shineslee <46121976+Shineslee@users.noreply.github.com>
2021-04-20 14:56:51 +0800

    Update jd_jxFactoryCreateTuan.js

--- dockerdiy/jd_jxFactoryCreateTuan.js
+++ dockerdiy/jd_jxFactoryCreateTuan.js
@@ -34,9 +34,9 @@ if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () =
       $.pickFriendEle = 0
 
       await TotalBean();
-      console.log(`\n******开始【京东账号${$.index}】${|| $.UserName}*********\n`);
+      console.log(`\n******开始【京东账号${$.index}】${$.UserName}*********\n`);
       if (!$.isLogin) {
-        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${|| $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
 
         if ($.isNode()) {
           await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
