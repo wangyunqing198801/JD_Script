f9a248ec42f21cd4e1caa0fda47fd3505e9a72ec
Shineslee <46121976+Shineslee@users.noreply.github.com>
2021-06-09 15:41:35 +0800

    Update jd_ShopSign.js

--- jd_ShopSign.js
+++ jd_ShopSign.js
@@ -53,6 +53,7 @@ if ($.isNode()) {
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
       $.index = i + 1;
       $.isLogin = true;
+      $.nickName = '';
       message = '';
       await TotalBean();
       console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
